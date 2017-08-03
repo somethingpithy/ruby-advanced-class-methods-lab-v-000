c60a94b4a6735c0ec12a4d6d73780a245cbc2928
Carlota Bindner <cev.bindner@gmail.com>
2017-08-03 02:53:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,27 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song_name = song.name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song_name = song.name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.include? { |name| song.name == song_name }
+
+  end
+
 end
